1531e0d9b4c2720541dd432f2506504a780e3657
kpolimis <kivan.polimis@gmail.com>
2016-07-27 18:47:07 -0700

    added indonesia mapping notebook, indonesian villages .png and updated bps_utils

--- notebooks/bps_utils.py
+++ notebooks/bps_utils.py
@@ -43,3 +43,67 @@ def get_HBS_data():
     zf = zipfile.ZipFile('Istat_HBS_2014.zip')
     file_handle = zf.open('FILENAME.csv')
     return pd.read_csv(file_handle)
+
+
+def to_xml(df, filename=None, mode='w'):
+    def row_to_xml(row):
+        xml = ['<item>']
+        for i, col_name in enumerate(row.index):
+            xml.append('  <field name="{0}">{1}</field>'.format(col_name,
+                                                                row.iloc[i]))
+        xml.append('</item>')
+        return '\n'.join(xml)
+    res = '\n'.join(df.apply(row_to_xml, axis=1))
+
+    if filename is None:
+        return res
+    with open(filename, mode) as f:
+        f.write(res)
+
+pd.DataFrame.to_xml = to_xml
+
+
+# Convenience functions for working with color ramps and bars
+def colorbar_index(ncolors, cmap, labels=None, **kwargs):
+    """
+    This is a convenience function to stop you making off-by-one errors
+    Takes a standard colourmap, and discretizes it,
+    then draws a colour bar with correctly aligned labels
+    """
+    cmap = cmap_discretize(cmap, ncolors)
+    mappable = cm.ScalarMappable(cmap=cmap)
+    mappable.set_array([])
+    mappable.set_clim(-0.5, ncolors+0.5)
+    colorbar = plt.colorbar(mappable, **kwargs)
+    colorbar.set_ticks(np.linspace(0, ncolors, ncolors))
+    colorbar.set_ticklabels(range(ncolors))
+    if labels:
+        colorbar.set_ticklabels(labels)
+    return colorbar
+
+
+def cmap_discretize(cmap, N):
+    """
+    Return a discrete colormap from the continuous colormap cmap.
+
+        cmap: colormap instance, eg. cm.jet.
+        N: number of colors.
+
+    Example
+        x = resize(arange(100), (5,100))
+        djet = cmap_discretize(cm.jet, 5)
+        imshow(x, cmap=djet)
+
+    """
+    if type(cmap) == str:
+        cmap = get_cmap(cmap)
+    colors_i = np.concatenate((np.linspace(0, 1., N), (0., 0., 0., 0.)))
+    colors_rgba = cmap(colors_i)
+    indices = np.linspace(0, 1., N + 1)
+    cdict = {}
+    for ki, key in enumerate(('red', 'green', 'blue')):
+        cdict[key] = [(indices[i], colors_rgba[i - 1, ki],
+                      colors_rgba[i, ki]) for i in xrange(N + 1)]
+
+    return matplotlib.colors.LinearSegmentedColormap(cmap.name + "_%d" % N,
+                                                     cdict, 1024)
